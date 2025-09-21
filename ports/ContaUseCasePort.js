dab408ae402a34a1cb247e92c25f075a1241ac2d
deyg <denilson.souza@live.com>
2025-09-20 22:51:07 -0300

    feat: interface ContaUseCasePort - run prompt 04

--- ports/ContaUseCasePort.js
+++ ports/ContaUseCasePort.js
@@ -1,1 +1,15 @@
- 
+// Porta de entrada do domínio para Casos de Uso
+// Interface baseada em classe abstrata (contrato comum: execute(input): output)
+
+export class ContaUseCasePort {
+  /**
+   * Executa o caso de uso.
+   * Implementações podem retornar sincrono ou Promise.
+   * @param {any} input - Dados de entrada do caso de uso.
+   * @returns {any} output - Resultado do caso de uso (valor ou Promise).
+   */
+  // eslint-disable-next-line no-unused-vars
+  execute(input) {
+    throw new Error('execute(input) não implementado');
+  }
+}
